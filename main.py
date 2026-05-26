002b060c8cfec615ecd2b268c6f0c46efa68e43c
Carine Neris <carine_neris@hotmail.com>
2026-05-25 22:42:03 -0300

    add description

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from config.database import engine, Base
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(
+    title="Zelda API",
+    description="API de jogos da franquia The Legend of Zelda",
+    version="1.0.0",
+)
 
 app.include_router(game_router)
